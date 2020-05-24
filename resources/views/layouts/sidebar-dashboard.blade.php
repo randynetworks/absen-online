2da83f77e58f6c014263f7b4f00f1006ef194e84
randynetworks <randynetworks@gmail.com>
2020-05-24 15:15:12 +0700

    add sidebar dan fix footer

--- resources/views/layouts/sidebar-dashboard.blade.php
+++ resources/views/layouts/sidebar-dashboard.blade.php
@@ -3,20 +3,25 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-      <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
-      </div>
-      <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-smile"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">ABSENSI HIMA/UKM</div>
     </a>
 
     <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Administrator
+    </div>
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-      <a class="nav-link" href="index.html">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
+    <li class="nav-item">
+        <a class="nav-link" href="/dashboard">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
     </li>
 
     <!-- Divider -->
@@ -24,39 +29,20 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-      Interface
+        Admin Management
     </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item - list admin account -->
     <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-        <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
-      </a>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Custom Components:</h6>
-          <a class="collapse-item" href="buttons.html">Buttons</a>
-          <a class="collapse-item" href="cards.html">Cards</a>
-        </div>
-      </div>
+        <a class="nav-link" href=/admin-profile">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Admin Profile</span></a>
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
+    <!-- Nav Item - list admin account -->
     <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
-      </a>
-      <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Custom Utilities:</h6>
-          <a class="collapse-item" href="utilities-color.html">Colors</a>
-          <a class="collapse-item" href="utilities-border.html">Borders</a>
-          <a class="collapse-item" href="utilities-animation.html">Animations</a>
-          <a class="collapse-item" href="utilities-other.html">Other</a>
-        </div>
-      </div>
+        <a class="nav-link" href="/admin-list">
+            <i class="fas fa-fw fa-users-cog"></i>
+            <span>List Admin Account</span></a>
     </li>
 
     <!-- Divider -->
@@ -64,41 +50,36 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-      Addons
+        Input Data
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-        <i class="fas fa-fw fa-folder"></i>
-        <span>Pages</span>
-      </a>
-      <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Login Screens:</h6>
-          <a class="collapse-item" href="login.html">Login</a>
-          <a class="collapse-item" href="register.html">Register</a>
-          <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-          <div class="collapse-divider"></div>
-          <h6 class="collapse-header">Other Pages:</h6>
-          <a class="collapse-item" href="404.html">404 Page</a>
-          <a class="collapse-item" href="blank.html">Blank Page</a>
+        <a class="nav-link" href="" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
+            aria-controls="collapsePages">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tables</span>
+        </a>
+        <div id="collapsePages" class="collapse hide" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Absensi:</h6>
+                <a class="collapse-item" href="/absen">Absen HIMA/UKM</a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">Other Input:</h6>
+                <a class="collapse-item" href="">Input Proker</a>
+                <a class="collapse-item" href="">Input Berita</a>
+            </div>
         </div>
-      </div>
     </li>
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-      <a class="nav-link" href="charts.html">
-        <i class="fas fa-fw fa-chart-area"></i>
-        <span>Charts</span></a>
-    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Nav Item - Tables -->
+    <!-- Nav Item - logout account -->
     <li class="nav-item">
-      <a class="nav-link" href="tables.html">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Tables</span></a>
+        <a class="nav-link" href="/logout" data-toggle="modal" data-target="#logoutModal">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>logout</span></a>
     </li>
 
     <!-- Divider -->
@@ -106,8 +87,8 @@
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
-      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
 
-  </ul>
-  <!-- End of Sidebar -->
+</ul>
+<!-- End of Sidebar -->
